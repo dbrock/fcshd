dbrock/fcshd

commit 087dc5dde3a95fd68820b07b3a93fb65a1bd797d

Automatically suggest import statements for standard components.

lib/fcshd/compiler.rb
@@ -1,11 +1,37 @@
+require "find"
+
 module FCSHD
   class Compiler
-    FCSH_EXECUTABLE = ENV["FCSH"] ||
-      if flex_home = ENV["FLEX_HOME"]
-        File.join(flex_home, "bin", "fcsh")
-      else
-        "fcsh"
+    FLEX_HOME = ENV["FLEX_HOME"]
+
+    def self.flex_path(*components)
+      File.join(FLEX_HOME, *components)
+    end
+
+    def self.standard_fcsh_executable
+      FLEX_HOME ? flex_path("bin", "fcsh") : "fcsh"
+    end
+
+    def self.standard_source_directory_root
+      flex_path("frameworks", "projects")
+    end
+
+    def self.barename(filename)
+      File.basename(filename).sub(/\..*/, "")
+    end
+
+    def self.find_standard_component(name)
+      if FLEX_HOME
+        Find.find(standard_source_directory_root) do |filename|
+          if barename(filename) == name
+            break File.dirname(filename).
+              sub(%r{.+/src/}, "").gsub("/", ".")
+          end
+        end
       end
+    end
+
+    FCSH_EXECUTABLE = ENV["FCSH"] ||
 
     def initialize(logger)
       @logger = logger

lib/fcshd/problem.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module FCSHD
   class Problem < Struct.new(:source_location, :raw_mxmlc_message)
     ERROR_PREFIX = /^Error: /
@@ -10,18 +11,35 @@ module FCSHD
       raw_mxmlc_message =~ ERROR_PREFIX
     end
 
+    def quote(string)
+      "‘#{string}’"
+    end
+
     def message
       case mxmlc_message
       when /^Unable to resolve MXML language version/
         <<"^D"
 Missing MXML version.
-  Try xmlns="http://www.adobe.com/2006/mxml
-   or xmlns:fx="library://ns.adobe.com/mxml/2009
+  Try  xmlns="http://www.adobe.com/2006/mxml       (for Flex 3)
+   or  xmlns:fx="library://ns.adobe.com/mxml/2009  (for Flex 4)
+
+Other useful namespaces for use in Flex 4 components:
+  xmlns:s="library://ns.adobe.com/flex/spark"
+  xmlns:mx="library://ns.adobe.com/flex/mx"
 ^D
-      when /^Could not resolve <(.+)> to a component implementation.$/
-        <<"^D"
-`#$1' is undefined.
+      when
+        /^Could not resolve <(.+)> to a component implementation.$/,
+        /^Call to a possibly undefined method (.+).$/
+      then
+        <<"^D".tap do |result|
+#{quote $1} undeclared
 ^D
+          Compiler.find_standard_component($1).tap do |package|
+            result << <<"^D" if package
+maybe #{quote "import #{package}.*"}?
+^D
+          end
+        end
       else
         mxmlc_message
       end

commit 6eead9fe703435d8fcfee4203f6f916a8789feee

Fix bug introduced by 087dc5dd.

lib/fcshd/compiler.rb
@@ -31,7 +31,7 @@ module FCSHD
       end
     end
 
-    FCSH_EXECUTABLE = ENV["FCSH"] ||
+    FCSH_EXECUTABLE = ENV["FCSH"] || standard_fcsh_executable
 
     def initialize(logger)
       @logger = logger