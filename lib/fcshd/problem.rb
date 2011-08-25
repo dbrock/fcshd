82779181185c11edce4f00e8545a7acc8a77ebdd
Daniel Brockman <daniel@gointeractive.se>
2011-08-25 17:30:50 +0200

    Recognize more error messages.

--- lib/fcshd/problem.rb
+++ lib/fcshd/problem.rb
@@ -25,9 +25,19 @@ missing MXML version
   xmlns="library://ns.adobe.com/flex/spark"
   xmlns:mx="library://ns.adobe.com/flex/mx"
 ^D
+      when /^Incorrect number of arguments.  Expected (\d+)\.$/
+        "expected #$1 arguments"
+      when /^(?:Access of possibly undefined property|Call to a possibly undefined method) (.+) through a reference with static type (.+)\.$/
+      then
+        <<"^D"
+#{quote $1} undeclared in #$2
+^D
       when
         /^Could not resolve <(.+)> to a component implementation.$/,
-        /^Call to a possibly undefined method (.+).$/
+        /^Call to a possibly undefined method (.+).$/,
+        /^Access of undefined property (.+).$/,
+        /^The definition of base class (.+) was not found.$/,
+        /^Type was not found or was not a compile-time constant: (.+)\.$/
       then
         <<"^D".tap do |result|
 #{quote $1} undeclared
@@ -38,18 +48,40 @@ missing MXML version
 ^D
           end
         end
+      when /^Implicit coercion of a value of type (.+) to an unrelated type (.+)\.$/
+        "expected #$2 (got #$1)"
+      when "Method marked override must override another method."
+        "overriding nonexistent method"
+      when "Overriding a function that is not marked for override."
+        <<"^D"
+unmarked override
+→ add override keyword
+^D
+      when "Incompatible override."
+        "incompatible override"
+      when /^Ambiguous reference to (.+)\.$/
+        "#{quote $1} is ambiguous"
+      when /^Warning: return value for function '(.+)' has no type declaration\.$/
+        "missing return type for #{quote $1}"
       else
         mxmlc_message
       end
     end
 
     def formatted_message_lines
-      message.lines.map do |line|
-        if error?
-          "error: #{line.chomp}"
-        else
-          line.chomp
+      lines = message.lines.entries
+      if error?
+        first = <<"^D"
+error: #{lines[0].chomp}
+^D
+        rest = lines[1..-1].map do |line|
+          <<"^D"
+       #{line.chomp}
+^D
         end
+        [first] + rest
+      else
+        lines
       end
     end
   end
