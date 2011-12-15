8bc47cc62fbc76ce6d798de679f73214a5010897
Daniel Brockman <daniel@brockman.se>
2011-12-15 23:30:45 +0100

    Detect and recover from infamous "Error: null".

--- lib/fcshd/compiler.rb
+++ lib/fcshd/compiler.rb
@@ -1,4 +1,4 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 module FCSHD
   class Compiler
     def initialize(logger)
@@ -8,13 +8,13 @@ module FCSHD
     end
 
     def start!
-      start_fcsh_process!
+      restart_fcsh_process!
     end
 
     def compile! command, frontend
       @command, @frontend = command, frontend
 
-      @logger.log("—" * 60)
+      log_horizontal_line
 
       if have_command_id?
         recompile!
@@ -25,7 +25,15 @@ module FCSHD
 
   private
 
-    def start_fcsh_process!
+    def log_horizontal_line
+      @logger.log("—" * 60)
+    end
+
+    def have_command_id?
+      @command_ids.include? @command
+    end
+
+    def restart_fcsh_process!
       stop_fcsh_process!
       @logger.log "starting #{FlexHome.fcsh}"
       @fcsh_process = IO.popen("#{FlexHome.fcsh} 2>&1", "r+")
@@ -44,41 +52,55 @@ module FCSHD
     def compile_new!
       @frontend.flush
       send_fcsh_command! @command
-      forward_compilation_output!
+      process_compilation_output!
     ensure
       if not have_command_id?
-        @logger.error "could not determine compilation ID"
-        @frontend.puts "fcshd: internal error; see fcshd log"
-        @frontend.flush
+        report_internal_error "could not determine compilation ID"
       end
     end
 
     def recompile!
       send_fcsh_command! "compile #{command_id}"
-      forward_compilation_output!
+      process_compilation_output!
+    rescue FCSHError => error
+      case error.message
+      when "Error: null"
+        @frontend.puts "fcshd: Adobe-internal error; restarting fcsh..."
+        restart_fcsh_process!
+        @frontend.puts "fcshd: retrying your compilation"
+        compile_new!
+      else
+        report_internal_error "unknown fcsh error: #{error.message}"
+      end
     end
 
     def command_id
       @command_ids[@command]
     end
 
-    def command_id= id
-      @command_ids[@command] = id
+    class FCSHError < StandardError; end
+
+    def report_internal_error(message)
+      @logger.error(message)
+      @frontend.puts "fcshd: internal error; see fcshd log"
+      @frontend.flush
     end
 
-    def have_command_id?
-      @command_ids.include? @command
-    end
-
-    def forward_compilation_output!
-      for line in @output.lines
+    def process_compilation_output!
+      for line in @output.lines.map(&:chomp)
         case line
         when /^fcsh: Assigned (\d+) as the compile target id$/
           self.command_id = $1
+        when "Error: null"
+          raise FCSHError, line
         else
           @frontend.puts(line)
         end
       end
+    end
+
+    def command_id= id
+      @command_ids[@command] = id
     end
 
     def send_fcsh_command! command
